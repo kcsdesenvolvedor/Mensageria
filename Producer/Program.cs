8ecc5ebf218066a461a9f9ae50301be4dbb47913
kcsdesenvolvedor <kcs.desenvolvedor@gmail.com>
2022-10-22 15:55:39 -0300

    Implementando a primeira mensagem no RabbitMQ

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(
+            queue: "myQueue",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+
+    string message = "Minha primeira mensagem!";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+            exchange: "",
+            routingKey: "myQueue",
+            basicProperties: null,
+            body: body
+        );
+    Console.ReadKey();
+}
